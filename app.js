734df2703574f7e0bffa88dd02e4dcc457fc0c00
Josh Suson <josh.suson@gmail.com>
2020-01-14 09:30:43 -0500

    Started project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('hello');
+});
+
+app.listen(3001, () => {
+    console.log('The Application is running!');
+});
