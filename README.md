ca28a7f81507c9b0a4d9b1ea90230716d111f3ae
Josh Suson <josh.suson@gmail.com>
2020-01-16 12:51:44 -0500

    finished project

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # FSJS-Project-6
  Project 6 for the Full Stack Tech Degeree
+
+ ### CSS Changes
+ **Global**
+ * Adjusted color palette
+ * Removed border and box shadow from picture
+
+ **Home Page**
+ * Removed border from images 
+ * Created a grow in animation for project thumbnails
+
+ **About Page**
+ * Adjust spacing for columns
+ * Redesigned skills list
+    * Added box
+    * Different background colors
+    * Changed font to white
+ * Increased font size for contact info
+
+ **Project Page**
+ * Redesigned skills list
+    * Adding box and spacing around
+    * Changed font color to white
+
--- routes/projects.js
+++ routes/projects.js
@@ -3,14 +3,13 @@ const router = express.Router();
 const {projects} = require('../data.json');
 
 
+router.get('/', (req, res) => {
+    res.redirect('/');
+})
+
 router.get('/:id', (req, res) => {
     const {id} = req.params;
-    const {project_name} = project[id];
-    const {description} = project[id];
-    const {technologies} = project[id];
-    const {live_link} = project[id];
-    const {github_link} = project[id];
-    const {image_urls} = project[id];
+    const {project_name, description, technologies, live_link, github_link, image_urls} = projects[id];
 
     const templateData = {id, project_name, description, technologies, live_link, github_link, image_urls};
     res.render('projects', templateData);
