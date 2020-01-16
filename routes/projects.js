0db47867bca7c9717ffc386e2f1a761e059de7ea
Josh Suson <josh.suson@gmail.com>
2020-01-15 21:20:25 -0500

    troubleshooting

--- routes/projects.js
+++ routes/projects.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 const {projects} = require('../data.json');
 
-router.get('/', (req, res) => {
-    res.redirect('/');
-})
 
 router.get('/:id', (req, res) => {
     const {id} = req.params;
